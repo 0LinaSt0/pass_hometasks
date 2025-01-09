546ccfbab70461121e6aee3d12bc68f765bfa373
msalena <polina@MacBook-Pro.local>
2025-01-09 21:18:12 +0300

    [advanced_python] sqler: added table FaceEncodings

--- advanced_python/utils/sqler.py
+++ advanced_python/utils/sqler.py
@@ -2,10 +2,12 @@ from sqlalchemy import (
     create_engine,
     Column,
     Integer,
-    String
+    String,
+    ForeignKey
 )
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 from utils.config import DATABASE_URL
 
@@ -27,6 +29,29 @@ class Photo(Base):
     filename = Column(String, index=True)
     filepath = Column(String, index=True)
     about = Column(String, index=True)
+
+    face_encodings = relationship(
+        'FaceEncodings', 
+        back_populates='photos',
+        cascade='all, delete-orphan'
+    )
+
+
+
+class FaceEncodings(Base):
+    '''
+    If there are some faces on one photo, where are added
+    every finded face as new row. So, one photo can have
+    more than one face_encodings.
+    '''
+    __tablename__ = 'face_encodings'
+
+    id = Column(Integer, primary_key=True, index=True)
+    photo_id = Column(Integer, ForeignKey('photo.id'), nullable=False)
+    encoding = Column(ARRAY(float), nullable=False)
+
+    photo = relationship('Photo', back_populates='face_encodings')
+
 
 
 def get_db():
