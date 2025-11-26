fdf30dd29c44771c7031e958e31de18f903dcda6
msalena <p.hristoforova@gmail.com>
2025-11-26 22:44:20 +0300

    [mlops] completed

---
+++ mlops/README.md
@@ -0,0 +1,62 @@
+# AIRFLOW SETUP
+
+## ~~~~~~~> 1. Virtualenv:
+```bash
+python3.8 -m venv .venv;
+source .venv/bin/activate;
+pip install --upgrade pip;
+```
+
+## ~~~~~~~> 2. Install and setup MySQL
+```bash
+sudo apt-get install mysql-server && sudo /etc/init.d/mysql start.;
+
+sudo mysql_secure_installation;
+sudo mysql -u root -p;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '<PASSWD>';
+
+sudo nano /etc/mysql/my.cnf;
+    # [mysqld]
+    # port = 33061
+
+sudo apt-get install build-essential \
+    && sudo apt-get install python3.8-dev libmysqlclient-dev \
+    && sudo apt install pkg-config;
+
+sudo service mysql restart;
+
+pip install mysqlclient;
+```
+
+And don't forget to create airflow user:
+```sql
+CREATE DATABASE Airflow;
+CREATE USER 'Airflow'@'%' IDENTIFIED WITH mysql_native_password BY -- <YOU PSWD>;
+GRANT USAGE, EXECUTE ON *.* TO 'Airflow'@'%';
+GRANT ALL PRIVILEGES ON Airflow.* TO 'Airflow'@'%';
+
+SELECT user, host FROM mysql.user WHERE user='Airflow';
+SHOW GRANTS FOR 'Airflow'@'localhost';
+```
+
+## ~~~~~~~> 3. Install and setup Airflow
+```bash
+pip install apache-airflow==2.7.3 --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.7.3/constraints-3.8.txt" --force-reinstall --upgradepip uninstall tenacity;
+
+cp sources/airflow.cfg /home/airflow/airflow.cfg;
+
+sudo mkdir /home/airflow && sudo mkdir /home/airflow/dags &&  sudo chmod -R 777 /home;
+export AIRFLOW_HOME=/home/airflow;
+airflow db migrate;
+
+airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin;
+```
+
+## ~~~~~~~> 4. Start Airflow
+```bash
+sudo service mysql restart &&\
+    export AIRFLOW_HOME=/home/airflow && \
+    (airflow scheduler & airflow webserver -p 8080);
+
+    # Look at http://localhost:8080 
+```
---
+++ mlops/mlops_dags/hw_DAG.py
@@ -0,0 +1,90 @@
+import sys
+
+# REPLACE TO YOUR PATH
+DATAPATH = '/home/msalena/SFMaga/main_repo/mlops/sources/files_hw_DE'
+
+sys.path.insert(0, DATAPATH)
+
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+import pendulum
+import os
+from pathlib import Path
+import pandas as pd
+from filelock import FileLock
+from transform_script import transfrom
+
+
+PROFIT_TABLE_FILE = Path(DATAPATH) / 'profit_table.csv'
+OUTPUT_FILE = Path(DATAPATH) / 'flags_activity.csv'
+LOCK_FILE = str(OUTPUT_FILE) + '.lock'
+PRODUCTS = list('abcdefghij')
+
+default_args = {
+    'owner': 'msalena',
+    'depends_on_past': False,
+    'start_date': pendulum.datetime(year=2024, month=1, day=5).in_timezone('Europe/Moscow'),
+    'retries': 1,
+}
+
+
+
+def save_results_safely(df_result):
+    '''For safe save'''
+    lock = FileLock(LOCK_FILE, timeout=10) 
+    with lock:
+        if os.path.exists(OUTPUT_FILE):
+            df_result.to_csv(OUTPUT_FILE, index=False, mode='a', header=False)
+        else:
+            df_result.to_csv(OUTPUT_FILE, index=False)
+
+
+def process_product(date=None, **context):
+    if not os.path.exists(PROFIT_TABLE_FILE):
+        print(f'Data file not found: {PROFIT_TABLE_FILE}')
+    else:
+        df = pd.read_csv(PROFIT_TABLE_FILE)
+
+        if df.empty:
+            print(f'Data file is empty: {PROFIT_TABLE_FILE}')
+        else:
+            if date is None:
+                # Get date for passing to the transfrom
+                date = context['logical_date']
+            
+            date_str = date.strftime('%Y-%m-%d')
+
+            print('DATE FOR CALCULATING:', date)
+
+            before_size = 0
+            if os.path.exists(OUTPUT_FILE):
+                before_size = pd.read_csv(OUTPUT_FILE).shape[0]
+                print('DF COUNT BEFORE:', before_size)
+            
+            df_result = transfrom(df, date_str)
+            
+            print('DF COUNT AFTER:', df_result.shape[0] + before_size)
+
+
+            # Save results
+            save_results_safely(df_result)
+
+
+with DAG(
+    'polina_khristoforova',
+    default_args=default_args,
+    schedule_interval='0 0 5 * *',  # every 5 day of every month at 12am
+    catchup=False,
+    max_active_runs=1,
+) as dag:
+
+    tasks = []
+    for product in PRODUCTS:
+        task = PythonOperator(
+            task_id=f'polina_khristoforova_{product}',
+            python_callable=process_product,
+            op_kwargs={'date': pendulum.datetime(year=2024, month=1, day=5).in_timezone('Europe/Moscow')},
+        )
+        tasks.append(task)
